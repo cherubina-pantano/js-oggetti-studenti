fa202103f8ea82f31fc17685ef8f198ed3b4d267
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-30 16:02:56 +0100

    Array di oggetti

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /*
 Descrizione
-- Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+1. - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
    Stampare attraverso il for..in tutte le proprietà.
-- Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
-- Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto
+2. - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+3. - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto
   studente all'array creato al punto 2 inserendo nell’ordine: nome, cognome e età.
 */
 
-// creazione e stampa di un oggetto
+// 1. creazione e stampa di un oggetto
 var studente = {
   nome : 'Ciccio',
   cognome: 'Pasticcio',
@@ -17,4 +17,29 @@ console.log(studente);
 
 for (var key in studente) {
   console.log(key, studente[key]);
+}
+
+// 2. Array di oggetti studenti
+var listaStudenti = [
+   {
+    nome: 'Marco',
+    cognome: 'Polo',
+    età: '26',
+  },
+   {
+    nome: 'Mario',
+    cognome: 'Rossi',
+    età: '25',
+  },
+   {
+    nome: 'Chiara',
+    cognome: 'Biachi',
+    età: '24',
+  },
+];
+console.log(listaStudenti);
+
+// Ciclo e stampo nome e cognome degli studenti
+for (var key in listaStudenti) {
+  console.log(listaStudenti[key].nome, listaStudenti[key].cognome);
 }
