e72b21d0f353e48ebb56f9054e962458376c653d
cherubina-pantano <cherubina.pantano@gmail.com>
2020-10-30 15:19:58 +0100

    Creazione e stampa di un oggetto js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('ook js');
+/*
+Descrizione
+- Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+   Stampare attraverso il for..in tutte le proprietà.
+- Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+- Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto
+  studente all'array creato al punto 2 inserendo nell’ordine: nome, cognome e età.
+*/
+
+// creazione e stampa di un oggetto
+var studente = {
+  nome : 'Ciccio',
+  cognome: 'Pasticcio',
+  età: 21,
+};
+console.log(studente);
+
+for (var key in studente) {
+  console.log(key, studente[key]);
+}
